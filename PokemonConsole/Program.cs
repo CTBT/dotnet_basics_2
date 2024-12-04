c0d0a4b29c50a14a3dce551de42a74df9f5271f4
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-12-04 20:01:04 +0100

    Update Program.cs

--- PokemonConsole/Program.cs
+++ PokemonConsole/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using PokemonConsole.Models;
+
 Console.WriteLine("Hello, World!");
+
+PokemonList? pokemonList = JsonSerializer
+    .Deserialize<PokemonList>(File.ReadAllText("testdata/pokemon.json"));
+var pokemonNames = pokemonList.results
+    .Select(i => i.name)
+    .ToList();
+
+    Console.WriteLine(string.Join(",", pokemonNames));
