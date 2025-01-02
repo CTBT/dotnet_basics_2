de3e1dd24f590d737fc9036231509a6b5d20a218
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2025-01-02 09:49:36 +0100

    level2
    level2

--- PokemonConsole/Program.cs
+++ PokemonConsole/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PokemonConsole.Models;
+
+using System.Text.Json;
+
+Console.WriteLine(" ----- Hello, this is the pokemon manual -----");
+
+// http call to get a list of pokemon from the pokemon api:
+var client = new HttpClient();
+const string url = "https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0";
+var response = await client.GetStringAsync(url);
+
+PokemonList? pokemonList;
+try
+{
+    var jsonOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true };
+    pokemonList = JsonSerializer.Deserialize<PokemonList>(response, jsonOptions);
+    ArgumentNullException.ThrowIfNull(pokemonList);
+}
+catch (Exception)
+{
+    Console.WriteLine($"Pokemon list could not be fetched from the api (Url: {url}");
+    throw;
+}
+
+
+// print the list of pokemon names:
+pokemonList.Results.ForEach(pokemon => Console.WriteLine(pokemon.Name));
