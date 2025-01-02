d8dcf5fafa1bd33028811918825e6e1bf952eb4b
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2025-01-02 17:14:23 +0100

    use typed results

--- PokemonWebApi/Endpoints/PokemonDataEndpoints.cs
+++ PokemonWebApi/Endpoints/PokemonDataEndpoints.cs
@@ -1,6 +1,9 @@
+using System.Net;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using PokemonLib.Models;
 using PokemonLib.Services;
+using Refit;
 
 namespace PokemonWebApi.Endpoints;
 
@@ -11,8 +14,21 @@ public static class PokemonDataEndpoints
         return await service.GetPokemonListAsync();
     }
     
-    public static async Task<Pokemon> GetPokemon([FromServices] PokemonService service, [FromRoute] string name)
+    public static async Task<Results<NotFound, Ok<Pokemon>>> GetPokemon([FromServices] PokemonService service, [FromRoute] string name)
     {
-        return await service.GetPokemonDetails(name);
+        Pokemon? pokemon = null;
+        try
+        {
+            pokemon = await service.GetPokemonDetails(name);
+        }
+        catch (ApiException e)
+        {
+            if (e.StatusCode == HttpStatusCode.NotFound)
+            {
+                return TypedResults.NotFound();
+            }
+        }
+
+        return TypedResults.Ok(pokemon);
     }
 }
