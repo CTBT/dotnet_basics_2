c7b0d2641836051b203a985790a3f0b3d4c8a78a
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2025-01-03 15:35:44 +0100

    level9

---
+++ PokemonLibTests/IntegrationTests/PokemonServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using PokemonLib.Services;
+
+namespace PokemonLibTests;
+
+public class PokemonServiceTests
+{
+    private readonly PokemonService _pokemonService;
+
+    public PokemonServiceTests()
+    {
+        _pokemonService = new PokemonService(NullLogger<PokemonService>.Instance);
+    }
+
+    [Fact]
+    public async Task GetPokemonListAsync_ReturnsPokemonList()
+    {
+        var result = await _pokemonService.GetPokemonListAsync();
+
+        Assert.True(result.Results.Count > 0);
+    }
+}
