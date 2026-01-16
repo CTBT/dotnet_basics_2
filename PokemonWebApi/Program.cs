9a09066f11497a28f0626162aa97d73003d88027
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2026-01-16 17:51:33 +0100

    add refit to di

--- PokemonWebApi/Program.cs
+++ PokemonWebApi/Program.cs
@@ -1,10 +1,14 @@
 using PokemonLib.Services;
 using PokemonWebApi.Endpoints;
+using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<PokemonService>();
-
+builder.Services.AddRefitClient<IPokemonApi>().ConfigureHttpClient(c =>
+{
+    c.BaseAddress = new Uri("https://pokeapi.co");
+});
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
